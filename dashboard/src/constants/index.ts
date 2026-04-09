fd8985f65c9dcef979842417c1c29d2940a37477
anvnh <73346280+anvnh@users.noreply.github.com>
2026-04-09 13:30:00 +0700

    refactor: add constants barrel export

---
+++ dashboard/src/constants/index.ts
@@ -0,0 +1,6 @@
+// Re-export all constants from a single entry point
+
+export { ROUTES, PAGE_TITLES } from './routes'
+export { APP_CONFIG } from './config'
+export { STATUS_COLORS, STATUS_ORDER } from './status'
+export { API_ENDPOINTS, API_HEADERS } from './api'
